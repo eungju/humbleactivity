4060c965464b8f5b76bc496374d0dfc9f403697b
blues <blues.park@kakaocorp.com>
2016-01-07 14:13:38 +0900

    Retrolambda 사용.

--- app/src/main/java/humbleactivity/app/ChainComposer.java
+++ app/src/main/java/humbleactivity/app/ChainComposer.java
@@ -1,9 +1,6 @@
 package humbleactivity.app;
 
-import rx.functions.Action1;
-
 import java.util.Collections;
-import java.util.List;
 
 public class ChainComposer {
     private final ChainComposerView view;
@@ -15,12 +12,9 @@ public class ChainComposer {
     }
 
     public void initialize() {
-        effectorService.listFilters().subscribe(new Action1<List<Filter>>() {
-            @Override
-            public void call(List<Filter> filters) {
-                view.setAvailableFilters(filters);
-                view.setChain(Collections.<Filter>emptyList());
-            }
+        effectorService.listFilters().subscribe(filters -> {
+            view.setAvailableFilters(filters);
+            view.setChain(Collections.<Filter>emptyList());
         });
     }
 }
