6be038ed87d87a03102122548c4a4c040a070893
Park Eungju <eungju@gmail.com>
2016-05-25 23:51:53 +0900

    Presenter 내 subscription 제거.

--- app/src/main/java/humbleactivity/app/ui/Counter.kt
+++ app/src/main/java/humbleactivity/app/ui/Counter.kt
@@ -6,21 +6,21 @@ import rx.Observable
 import rx.functions.Action1
 
 class Counter {
-    private val current = BehaviorRelay.create<Int>()
+    private val count = BehaviorRelay.create<Int>()
     private val up = PublishRelay.create<Unit>()
     private val down = PublishRelay.create<Unit>()
-    private val currentUpdate = current.mergeWith(up.withLatestFrom(current, { a, b -> b + 1 })
-            .mergeWith(down.withLatestFrom(current, { a, b -> b - 1 }))
-            .doOnNext { current.call(it) }
+    private val stateUpdate = count.mergeWith(up.withLatestFrom(count, { up, count -> count + 1 })
+            .mergeWith(down.withLatestFrom(count, { down, count -> count - 1 }))
+            .doOnNext { count.call(it) }
             .ignoreElements())
 
-    fun current(): Observable<Int> = currentUpdate
+    fun current(): Observable<Int> = stateUpdate
 
     fun onUp(): Action1<Unit> = up
 
     fun onDown(): Action1<Unit> = down
 
     fun initialize(initial: Int) {
-        current.call(initial)
+        count.call(initial)
     }
 }
