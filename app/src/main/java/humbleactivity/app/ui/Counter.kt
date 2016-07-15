b51a9f143dc7f43feee6b06c506a5d5ef3cd059c
Park Eungju <eungju@gmail.com>
2016-07-16 00:16:42 +0900

    다시 State.

--- app/src/main/java/humbleactivity/app/ui/Counter.kt
+++ app/src/main/java/humbleactivity/app/ui/Counter.kt
@@ -5,12 +5,19 @@ import rx.Observable
 import rx.functions.Action1
 
 class Counter(initial: Int) {
+    data class State(val count: Int) {
+        fun up(): State = copy(count = count + 1)
+
+        fun down(): State = copy(count = count - 1)
+    }
+
     private val up = PublishRelay.create<Unit>()
     private val down = PublishRelay.create<Unit>()
-    private val count = up.map { 1 }.mergeWith(down.map { -1 })
-            .scan(initial, { count, delta -> count + delta })
+    private val state = up.map { { state: State -> state.up() } }
+            .mergeWith(down.map { { state: State -> state.down() } })
+            .scan(State(initial), { state, action -> action(state) })
 
-    fun count(): Observable<Int> = count
+    fun count(): Observable<Int> = state.map { it.count }
 
     fun onUp(): Action1<Unit> = up
 
