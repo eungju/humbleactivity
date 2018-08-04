d8564b312fab2c324df5fca71e1cf8d1999f2f8e
Park Eungju <eungju@gmail.com>
2018-08-04 19:17:16 +0900

    Use Kotlin Android Extensions.

--- app/src/main/java/humbleactivity/app/ui/ChainComposerActivity.kt
+++ app/src/main/java/humbleactivity/app/ui/ChainComposerActivity.kt
@@ -2,18 +2,14 @@ package humbleactivity.app.ui
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
 import android.widget.ArrayAdapter
-import android.widget.ListView
 import android.widget.Toast
-
-import butterknife.ButterKnife
 import com.jakewharton.rxbinding2.view.RxView
 import humbleactivity.app.HumbleApplication
 import humbleactivity.app.R
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-
+import kotlinx.android.synthetic.main.activity_chain_composer.*
 import javax.inject.Inject
 
 class ChainComposerActivity : Activity() {
@@ -24,21 +20,14 @@ class ChainComposerActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         HumbleApplication.get(this).component().inject(this)
-        subscriptions = CompositeDisposable()
 
         setContentView(R.layout.activity_chain_composer)
-        val availablesView = ButterKnife.findById<ListView>(this, R.id.availables)
         val availablesAdapter = ArrayAdapter<String>(this, R.layout.view_filter_item)
-        availablesView.adapter = availablesAdapter
-        val chainView = ButterKnife.findById<ListView>(this, R.id.chain)
+        availables.adapter = availablesAdapter
         val chainAdapter = ArrayAdapter<String>(this, R.layout.view_filter_item)
-        chainView.adapter = chainAdapter
-        val refreshView = ButterKnife.findById<View>(this, R.id.refresh)
-        val addToChainView = ButterKnife.findById<View>(this, R.id.add_to_chain)
-        val removeFromChainView = ButterKnife.findById<View>(this, R.id.remove_from_chain)
-        val moveDownView = ButterKnife.findById<View>(this, R.id.move_down)
-        val moveUpView = ButterKnife.findById<View>(this, R.id.move_up)
+        chain.adapter = chainAdapter
 
+        subscriptions = CompositeDisposable()
         //Output signals
         subscriptions.add(presenter.availables
                 .observeOn(AndroidSchedulers.mainThread())
@@ -55,7 +44,7 @@ class ChainComposerActivity : Activity() {
         subscriptions.add(presenter.chainCursor
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe { position ->
-                    chainView.setItemChecked(position, true)
+                    chain.setItemChecked(position, true)
                 })
         subscriptions.add(presenter.loadError
                 .observeOn(AndroidSchedulers.mainThread())
@@ -63,19 +52,19 @@ class ChainComposerActivity : Activity() {
                     Toast.makeText(this, message, Toast.LENGTH_LONG).show()
                 })
         //Input signals
-        subscriptions.add(RxView.clicks(refreshView).map { Unit }
+        subscriptions.add(RxView.clicks(refresh).map { Unit }
                 .subscribe(presenter.refresh))
-        subscriptions.add(RxView.clicks(addToChainView).map { availablesView.checkedItemPosition }
-                .filter { p -> p >= 0 && p < availablesView.count }
+        subscriptions.add(RxView.clicks(add_to_chain).map { availables.checkedItemPosition }
+                .filter { p -> p >= 0 && p < availables.count }
                 .subscribe(presenter.addToChain))
-        subscriptions.add(RxView.clicks(removeFromChainView).map { chainView.checkedItemPosition }
-                .filter { p -> p >= 0 && p < chainView.count }
+        subscriptions.add(RxView.clicks(remove_from_chain).map { chain.checkedItemPosition }
+                .filter { p -> p >= 0 && p < chain.count }
                 .subscribe(presenter.removeFromChain))
-        subscriptions.add(RxView.clicks(moveUpView).map { chainView.checkedItemPosition }
-                .filter { p -> p >= 1 && p < chainView.count }
+        subscriptions.add(RxView.clicks(move_up).map { chain.checkedItemPosition }
+                .filter { p -> p >= 1 && p < chain.count }
                 .subscribe(presenter.moveUp))
-        subscriptions.add(RxView.clicks(moveDownView).map { chainView.checkedItemPosition }
-                .filter { p -> p >= 0 && p < chainView.count - 1 }
+        subscriptions.add(RxView.clicks(move_down).map { chain.checkedItemPosition }
+                .filter { p -> p >= 0 && p < chain.count - 1 }
                 .subscribe(presenter.moveDown))
 
         presenter.initialize()
--- app/src/main/java/humbleactivity/app/ui/CounterActivity.kt
+++ app/src/main/java/humbleactivity/app/ui/CounterActivity.kt
@@ -2,16 +2,12 @@ package humbleactivity.app.ui
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
-
-import butterknife.ButterKnife
 import com.jakewharton.rxbinding2.view.RxView
 import com.jakewharton.rxbinding2.widget.RxTextView
 import humbleactivity.app.R
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-
+import kotlinx.android.synthetic.main.activity_counter.*
 import javax.inject.Inject
 
 class CounterActivity : Activity() {
@@ -22,19 +18,15 @@ class CounterActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        subscriptions = CompositeDisposable()
-
         setContentView(R.layout.activity_counter)
-        val currentView = ButterKnife.findById<TextView>(this, R.id.current)
-        val upView = ButterKnife.findById<View>(this, R.id.up)
-        val downView = ButterKnife.findById<View>(this, R.id.down)
 
         presenter = Counter(0)
+        subscriptions = CompositeDisposable()
         subscriptions.add(presenter.count.map { it.toString() }
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(RxTextView.text(currentView)))
-        subscriptions.add(RxView.clicks(upView).map { Unit }.subscribe(presenter.up))
-        subscriptions.add(RxView.clicks(downView).map { Unit }.subscribe(presenter.down))
+                .subscribe(RxTextView.text(current)))
+        subscriptions.add(RxView.clicks(up).map { Unit }.subscribe(presenter.up))
+        subscriptions.add(RxView.clicks(down).map { Unit }.subscribe(presenter.down))
     }
 
     override fun onDestroy() {
